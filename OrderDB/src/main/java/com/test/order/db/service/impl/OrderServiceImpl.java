395352fc7461a7a2613b92bf5b9b841697aca057
十二月的波爵 <1532558305@qq.com>
2015-08-23 21:53:37 +0800

    add

---
+++ OrderDB/src/main/java/com/test/order/db/service/impl/OrderServiceImpl.java
@@ -0,0 +1,11 @@
+package com.test.order.db.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.test.order.db.entiy.Orders;
+import com.test.order.db.service.OrderService;
+@SuppressWarnings("unchecked")
+@Service
+public class OrderServiceImpl extends BaseServiceImpl<Orders> implements OrderService{
+
+}
